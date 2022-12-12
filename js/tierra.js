de3987584d48e05f178529d62b6723225fe13027
yesseyela <yesseyela@gmail.com>
2022-12-11 20:25:01 -0500

    sexto commit

--- js/tierra.js
+++ js/tierra.js
@@ -1,32 +1,23 @@
-// Función para calcular el ángulo recorrido en grados, minutos y segundos
-function calcularArcoRecorrido(horas, minutos, segundos) {
-    // Validamos que las horas, minutos y segundos sean valores válidos
-    if (horas < 0 || horas > 23) {
-      return "Error: las horas deben estar entre 0 y 23";
-    }
-    if (minutos < 0 || minutos > 59) {
-      return "Error: los minutos deben estar entre 0 y 59";
-    }
-    if (segundos < 0 || segundos > 59) {
-      return "Error: los segundos deben estar entre 0 y 59";
-    }
-  
-    // Convertimos las horas a grados
-    let gradosHoras = horas * 30;
-    // Convertimos los minutos a grados
-    let gradosMinutos = minutos * 0.5;
-    // Convertimos los segundos a grados
-    let gradosSegundos = segundos * 0.0083333333333;
-  
-    // Calculamos el ángulo total recorrido en grados
-    let gradosTotales = gradosHoras + gradosMinutos + gradosSegundos;
-  
-    // Calculamos el ángulo total en minutos y segundos
-    let minutosTotales = Math.floor(gradosTotales * 60);
-    let segundosTotales = Math.floor((gradosTotales * 60 - minutosTotales) * 60);
-  
-    // Devolvemos el resultado en grados, minutos y segundos
-    return gradosTotales + " grados, " + minutosTotales + " minutos y " + segundosTotales + " segundos";
-  }
-  
-  
+function convertir() {
+    var horas = parseFloat(document.getElementById("txt_horas").value);
+    var minutos = parseFloat(document.getElementById("txt_minutos").value);
+    var segundos = parseFloat(document.getElementById("txt_segundos").value);
+
+    if (horas >= 0 && minutos >= 0 && segundos >= 0) {
+
+        // Convertimos los segundos a minutos
+        let min_seg = segundos /60;
+        // Convertimos los minutos a horas
+        minutos = minutos + min_seg;
+        let horas_min = minutos /60;
+        // Convertimos horas a grados
+        horas = horas + horas_min;
+        
+        grados = horas * 360 / 24;
+        
+        document.getElementById("resultado").innerHTML = "La tierra ha dado un giro de: " + grados + " grados";
+        
+    } else {
+        alert("Cantidad no valida");
+    }  
+}
